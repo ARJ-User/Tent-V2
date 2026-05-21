810883484c5fd7e69e501cfb21d13bf42b3f3d33
MD ANISUR RAHMAN <arjubel8@gmail.com>
2026-05-21 13:54:53 +0600

    Remove menu toggle event listener and related code
    Remove menu toggle functionality from script.js.

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-  const menuToggle = document.querySelector(".menu-toggle");
-  const navLinks = document.querySelector(".nav-links");
 
-  menuToggle.addEventListener("click", () => {
-    menuToggle.classList.toggle("open");
-    navLinks.classList.toggle("active");
-  });
